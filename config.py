2061c05566514e06b37c870333f08a9798c01ccb
Cherimolah <inkustregister@mail.ru>
2024-06-23 19:33:10 +0500

    Fix .env supply

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN").split(";")
-USER_TOKENS = os.environ.get("USER_TOKENS").split(";")
+BOT_TOKEN = os.environ.get("BOT_TOKEN").split(",")
+USER_TOKENS = os.environ.get("USER_TOKENS").split(",")
 
 USER = os.environ.get("PG_USER")
 HOST = os.environ.get("HOST")
@@ -17,7 +17,7 @@ PASSWORD = os.environ.get("PASSWORD")
 DATABASE = os.environ.get("DATABASE")
 
 OWNER = int(os.environ.get("OWNER"))
-ADMINS = list(map(int, os.environ.get("ADMINS").split(";")))
+ADMINS = list(map(int, os.environ.get("ADMINS").split(",")))
 
 DATETIME_FORMAT = os.environ.get("DATETIME_FORMAT")
 
