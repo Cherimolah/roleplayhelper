61758a247fec28e853631192654a95fba358ede5
Cherimolah <inkustregister@mail.ru>
2025-01-22 04:42:28 +0500

    Fix approve bug

--- handlers/group_events.py
+++ handlers/group_events.py
@@ -3,7 +3,6 @@ import random
 
 from vkbottle import GroupEventType, API
 from vkbottle_types.events.bot_events import GroupJoin
-from vkbottle_types.objects import UsersFields
 
 from loader import bot, users
 import messages
@@ -18,8 +17,7 @@ async def group_join(event: GroupJoin):
     await user.groups.approve_request(group_id=event.group_id, user_id=event.object.user_id)
     await asyncio.sleep(0.34)
     can_write = (await user.users.get(user_ids=[event.object.user_id],
-                                      fields=[UsersFields.CAN_WRITE_PRIVATE_MESSAGE]))[0].can_write_private_message
+                                      fields=['can_write_private_message']))[0].can_write_private_message  # type: ignore
     await asyncio.sleep(0.34)
     if can_write:
-        await user.messages.send(user_id=event.object.user_id, random_id=0,
-                                 message=messages.accepted_to_group)
+        await user.messages.send(peer_id=event.object.user_id, message=messages.accepted_to_group, random_id=0)
