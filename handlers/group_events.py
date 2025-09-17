e3b22437682948eb8cd85314c6d5bd0cf13b977f
Cherimolah <inkustregister@mail.ru>
2025-09-18 01:31:50 +0500

    Add some features

--- handlers/group_events.py
+++ handlers/group_events.py
@@ -14,7 +14,7 @@ async def group_join(event: GroupJoin):
         return
     await user_bot.api.groups.approve_request(group_id=event.group_id, user_id=event.object.user_id)
     await asyncio.sleep(0.34)
-    can_write = (await user_bot.users.get(user_ids=[event.object.user_id],
+    can_write = (await user_bot.api.users.get(user_ids=[event.object.user_id],
                                       fields=['can_write_private_message']))[0].can_write_private_message  # type: ignore
     await asyncio.sleep(0.34)
     if can_write:
